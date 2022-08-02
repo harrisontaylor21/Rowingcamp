6714a33c2d51acb961aff4db4b0bfa407680b8f7
Harrison Taylor <htfarrell003@gmail.com>
2022-08-02 18:38:57 -0400

    implemented rower class, wrote preliminary lineup

---
+++ lineups.py
@@ -0,0 +1,7 @@
+# Harrison Taylor
+# Aug 1, 2022
+# make lineups using a list of rower objects, to fill out n lineups
+def make_lineups(list, n):
+    for rower in list:
+        if rower.side == port:
+
---
+++ rower_class.py
@@ -0,0 +1,13 @@
+# Harrison Taylor
+# August 2, 2022
+# rower class
+
+# rowers have a side, a height, and a 2k erg score
+class Rower:
+    def __init__(self, name, side, height, erg):
+        self.name = name
+        self.side = side
+        self.height = height
+        self.erg = erg
+
+    
